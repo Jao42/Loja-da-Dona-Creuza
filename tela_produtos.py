768ed3a27eab6d031c4b4d0e3668b8964cebcb41
Jao42 <joaovictormedeiros41@gmail.com>
2021-04-08 21:19:34 -0300

    tela_produtos.py

--- tela_produtos.py
+++ tela_produtos.py
@@ -14,41 +14,56 @@ def telaProdutos():
   y = int((screenHeight/2) - (height/2))
 
   tela.geometry(f'{width}x{height}+{x}+{y}')
-  tela.title('Loja da Dona Creuza')
+  tela.title('Estoque')
 
   #relief --> estilo da borda
 
-  frameTitulo = Frame(tela, borderwidth=2, width=400, height=2) 
-  titulo = Label(frameTitulo, text='PRODUTOS')
-
-  frameTitulo.pack(side=TOP)
-  titulo.pack()
-
   body = Frame(tela, borderwidth=2, relief='solid') 
 
-  tabelaItems=['nome', 'preço', 'quantidade']
+  tabelaItems=['nome', 'preço']
 
 #selectmode -> quantos items seleciona com mouse
-
-
 #treeView heigth -> quantas rows
-  tree = ttk.Treeview(body, columns=(tabelaItems[0], tabelaItems[1], tabelaItems[2]), selectmode="extended")
+  scrollbarY = Scrollbar(body, orient=VERTICAL)
+  scrollbarX = Scrollbar(body, orient=HORIZONTAL)
+
+  tree = ttk.Treeview(
+      body, columns=(tabelaItems[0], tabelaItems[1]), selectmode="extended", height=5,
+      yscrollcommand=scrollbarY.set, xscrollcommand=scrollbarX.set
+      )
+
+  scrollbarY.config(command=tree.yview)
+  scrollbarX.config(command=tree.xview)
+  scrollbarY.pack(side=RIGHT, fill=Y)
+  scrollbarX.pack(side=BOTTOM, fill=X)
+
+  style = ttk.Style(body)
+  style.configure('Treeview', rowheight=60)
 
   tree.heading(tabelaItems[0], text='produto')
   tree.heading(tabelaItems[1], text='preço')
-  tree.heading(tabelaItems[2], text='quantidade')
 
-#inicializacao da tabela(exotico, neh?).
-  tree.column('#0', stretch=NO, minwidth=0, width=0)
 
 #tabela
-  tree.column('#1', stretch=1, minwidth=0, width=200)
-  tree.column('#2', stretch=1, minwidth=0, width=200)
-  tree.column('#3', stretch=1, minwidth=0, width=200)
+  tree.column('#0', minwidth=0, width=100)
+  tree.column('#1', anchor="center", minwidth=0, width=200)
+  tree.column('#2', anchor="center", minwidth=0, width=200)
   
-  imagem = PhotoImage(file="./imgs/dona-creuza-peq.png")
+  imagem = PhotoImage(file="./imgs/uno.png")
 
-  tree.insert('', 'end', text='fdd', open=True, image=imagem, values=('Uno duas portas', '20', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '20', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '21', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '22', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '23', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '24', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '25', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '26', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '27', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '28', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '29', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '30', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '31', '1'))
+  tree.insert('', 'end', image=imagem, values=('Uno duas portas seminovo', '32', '1'))
   body.pack()
   tree.pack(side=BOTTOM)
 

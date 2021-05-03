2496295e06fb7c891e975e2ea96bd557f93bac11
Jao42 <joaovictormedeiros41@gmail.com>
2021-05-03 09:22:47 -0300

    nova tela de login

--- tela_login.py
+++ tela_login.py
@@ -1,38 +1,69 @@
 from tkinter import *
 from login import *
-from centralizar_tela import *
 import config
 
 def telaLogin():
-  tela_login = Toplevel() 
-  tela_login.configure(bg=config.COR_BG)
-
-  centralizarTela(tela_login, 600, 500)
-
+  tela_login = Toplevel()
+  tela_login.configure(bg=config.COR_BG2)
   tela_login.title("Login")
+  tela_login.geometry('1366x768')
+
+  # CRIANDO FRAME DA ESQUERDA
+  Left = Frame(tela_login, width=650, height=900, bg=config.COR_BG3)
+  Left.pack(side=LEFT)
+
+  # CRIANDO FRAME A DIREITA
+  Right = Frame(tela_login, width=750, height=900, bg=config.COR_BG)
+  Right.pack(side=RIGHT)
+
+  # INSERINDO ELEMENTOS NA FRAME DA ESQUERDA
+  img = PhotoImage(file='./imgs/circulo-superior-preto.png')
+  lblimg = Label(Left, image=img, bg=config.COR_BG3)
+  lblimg.place(x=0, y=0)
+
+  lbl1 = Label(Left, font='Arial 28 bold', bg=config.COR_BG3, fg=config.COR_BG2, justify=LEFT,
+               text='É ÓTIMO TER VOCÊ AQUI\nMAIS UMA VEZ!')
+  lbl1.place(x=60, y=280)
+
+  lbl2 = Label(Left, font='Arial 13', bg=config.COR_BG3, fg=config.COR_BG2, justify=LEFT,
+               text='FAÇA SEU LOGIN PARA FAZER SUA NOVA COMPRA.')
+  lbl2.place(x=60, y=380)
+
+  # INSERINDO ELEMENTOS NA FRAME DA DIREITA
+  logo = PhotoImage(file='./imgs/logotipo.preto.png')
+  lbllogo = Label(Right, image=logo, bg=config.COR_BG)
+  lbllogo.place(x=200, y=80)
+
+  img2 = PhotoImage(file='./imgs/onda.png')
+  lblimg2 = Label(Right, image=img2, bg=config.COR_BG)
+  lblimg2.place(x=15, y=355)
 
   email_usuario = StringVar()
   senha = StringVar()
 
-  label_email_usuario = Label(tela_login, text="Digite o seu email ou usuário: ", height="3", font="Arial 12", bg=config.COR_BG)
+  label_email_usuario = Label(Right, text='DIGITE O SEU E-MAIL OU USUÁRIO:', font='Arial 14', bg=config.COR_BG, fg=config.COR_BG3)
+  label_senha = Label(Right, text='DIGITE SUA SENHA:', font='Arial 14', bg=config.COR_BG, fg=config.COR_BG3)
+  email_usuario_entrada = Entry(Right, textvariable=email_usuario)
+  senha_entrada = Entry(Right, textvariable=senha, show="*")
 
-  label_senha = Label(tela_login, text="Digite a sua senha: ", height="3", font='Arial 12', bg=config.COR_BG)
+  label_email_usuario.place(x=60, y=285)
+  email_usuario_entrada.place(x=60, y=315, width=400, height=25)
 
-  email_usuario_entrada = Entry(tela_login, textvariable=email_usuario, width="40")
-  senha_entrada = Entry(tela_login, textvariable=senha, width="25", show="*")
+  label_senha.place(x=60, y=360)
+  senha_entrada.place(x=60, y=390, width=400, height=25)
 
-  label_email_usuario.place(x=30, y=30)
-  email_usuario_entrada.place(x=30, y=70)
-
-  label_senha.place(x=30, y=90)
-  senha_entrada.place(x=30, y=130)
-
-  labelMensagem = Label(tela_login, bg=config.COR_BG)
+  labelMensagem = Label(Right, bg=config.COR_BG)
   labelMensagem['bg'] = config.COR_BG
-  labelMensagem.pack()
+  labelMensagem.place(x=200, y=10)
 
-  Button(tela_login, text="Login", height="2", width="30", command=lambda: displayVerifLogin(email_usuario, senha, tela_login, labelMensagem)).pack(side=BOTTOM)
-
+  bttn = Button(Right,
+                text='Login',
+                font='Arial 13',
+                bg=config.COR_BG2,
+                height=2,
+                width=20,
+                command=lambda: displayVerifLogin(email_usuario, senha, tela_login, labelMensagem))
+  bttn.place(x=160, y=480)
   tela_login.mainloop()
 
 

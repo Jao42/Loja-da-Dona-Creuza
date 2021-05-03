b81becbb4f867e20984c0568b4849f6cad014b0f
Jao42 <joaovictormedeiros41@gmail.com>
2021-05-03 09:20:58 -0300

    nova tela inicial

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
 COR_BG = '#9ddde0'
+COR_BG2 = '#ffffff'
+COR_BG3 = '#000000'
--- tela_inicial.py
+++ tela_inicial.py
@@ -7,52 +7,55 @@ import os
 
 def telaInicial():
   tela_inicial = Tk()
-  centralizarTela(tela_inicial, 600, 500)
+  tela_inicial.geometry('1366x768')
 
   tela_inicial.title("Lojinha da Dona Creuza :)")
   tela_inicial.configure(bg=config.COR_BG)
 
-  Label(text="Bem Vindo a Lojinha da Dona Creuza!",
-      bg=config.COR_BG,
-      height="3",
-      font='Calibrin 19 bold').place(x=30, y=30)
+  # CRIANDO FRAME A ESQUERDA
+  Left = Frame(tela_inicial, width=700, height=768, bg=config.COR_BG3)
+  Left.pack(side=LEFT)
 
-  imagem = PhotoImage(file="./imgs/dona-creuza-peq.png")
+  # CRIANDO FRAME A DIREITA
+  Right = Frame(tela_inicial, width=666, height=768, bg=config.COR_BG3)
+  Right.pack(side=RIGHT)
 
-  Label(image=imagem,
-      bg=config.COR_BG,
-      bd=2,
-      relief='solid').place(x=40, y=150)
+  # INSERINDO DESIGN NO FRAME DA DIREITA
+  img = PhotoImage(file="./imgs/design.png")
+  lbldesign = Label(Right, image=img)
+  lbldesign.pack()
 
-  Label(text="Essa é a Dona Creuza :3", 
-      fg="Black", 
-      bg=config.COR_BG,
-      font='Arial 11 italic').place(x=27, y=360)
+  # INSERINDO IMAGEM NO FRAME DA ESQUERDA
+  img2 = PhotoImage(file="./imgs/circulo-inferior-preto.png")
+  lblimg = Label(Left, image=img2, bg=config.COR_BG3)
+  lblimg.place(x=0, y=380)
 
 
-  Label(text="Se você ainda não se registrou, clique em \"Registrar\"", 
-    bg=config.COR_BG, 
-    font='Arial 12', 
-    anchor=CENTER).place(x=200, y=170)
+  # INSERINDO TEXTO E LOGO NO FRAME DA ESQUERDA
+  logo = PhotoImage(file="./imgs/logotipo-grande.png")
+  lbllogo = Label(Left, image=logo, bg=config.COR_BG3)
+  lbllogo.place(x=0, y=20)
 
-  Label(text="Se já se registrou, clique em \"Login\"",
-      bg=config.COR_BG,
-      font='Arial 11',
-      anchor=CENTER).place(x=250, y=195)
+  Label(Left, text=
+  """
+  A LOJINHA DA DONA CREUZA É UMA LOJA VIRTUAL DE VARIEDADES, AQUI
+  VOCÊ ENCONTRARÁ DE PRODUTOS ELETRÔNICOS A ALIMENTOS.
+  POSSUÍMOS MARCAS DE QUALIDADE E NOSSO ATENDIMENTO É PENSADO
+  NO BEM ESTAR DOS NOSSOS CLIENTES, ALÉM DE POSSUÍMOS PROMOCOES
+  QUE VOCÊ NÃO ENCONTRARÁ EM OUTRO LUGAR!
+  TÁ ESPERANDO O QUE MENINO? VENHA SER NOSSO CLIENTE!
+  """,
+        bg=config.COR_BG3,
+        fg=config.COR_BG,
+        font='Arial 12',
+        justify=LEFT).place(x=50, y=300)
 
-  Button(text="Registrar",
-      height="2",
-      width="30",
-      bg="#ffffcc",
-      command=telaCadastro).place(x=260, y=230)
+  # INSERINDO BUTTON NO FRAME DA ESQUERDA 
+  bnt1 = Button(Left, text='Entrar', font='Arial 13', bg=config.COR_BG2, command=telaCadastro, width=13, height=2)
 
-  Button(text="Login",
-      height="2",
-      width="30",
-      bg="#ffffcc",
-      command=telaLogin).place(x=260, y=270)
-
+  bnt1.place(x=230, y=450)
   tela_inicial.mainloop()
+
 
 
 telaInicial()

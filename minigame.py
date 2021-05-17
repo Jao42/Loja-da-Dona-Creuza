75781e02d7ce868d27d69485b890daabbf017057
Jao42 <joaovictormedeiros41@gmail.com>
2021-05-17 07:19:05 -0300

    remake minigame

--- minigame.py
+++ minigame.py
@@ -1,20 +1,24 @@
 from operacoes_db import atualizarPontos, pegarPontos
+from random import randint
+import config
 
-def aumentarProgresso(progresso, labelPontos, labelProdutos, usuario):
+def aumentarProgresso(progresso, tasks, labelPontos, labelProdutos, labelTask, usuario):
   progresso['value'] += 20
-  if progresso['value'] == 100:
-    
-    pontos = pegarPontos(usuario)
-    pontos += 50
-    atualizarPontos(usuario, pontos)
-    labelProdutos['text'] = f'{usuario} --> {pontos}'
-
-    progresso['value'] = 0
-    labelPontos['text'] = '+50 CreuzaPoints!'
 
   if progresso['value'] == 20:
     labelPontos['text'] = ''
+    return 0
 
+  if progresso['value'] == 100:
+    pontos = pegarPontos(usuario)
+    pontos += 50
+    atualizarPontos(usuario, pontos)
+    indexTask = randint(0, 4)
+    labelTask['text'] = f'Ajude a Dona Creuza a {config.TASKS[indexTask]}'
+    labelProdutos['text'] = f'{usuario} --> {pontos}'
+    progresso['value'] = 0
+    labelPontos['text'] = '+50 CreuzaPoints!'
   return 0
+
 
 
--- tela_minigame.py
+++ tela_minigame.py
@@ -1,21 +1,34 @@
 from tkinter import *
 from tkinter import ttk
+import config
 from operacoes_db import pegarPontos, atualizarPontos
 from minigame import aumentarProgresso
 
 def telaMinigame(usuario, labelProdutos):
   tela = Toplevel()
   tela.geometry("600x500")
+  tela.title('Minigame')
+  tela.configure(bg=config.COR_BG)
 
-  task = Label(tela, text="Ajude a Dona Creuza com as compras!", pady=50)
-  task.pack()
-  progresso = ttk.Progressbar(tela, orient=HORIZONTAL,length=200,mode='determinate')
-  progresso.pack()
-  labelPontos = Label(tela, text='')
-  clickBotao = Button(tela, text='CLIQUE!', command=lambda:aumentarProgresso(progresso, labelPontos, labelProdutos, usuario))
-  clickBotao.pack(anchor=CENTER)
-  labelPontos.pack()
+  tela.resizable(False, False)
+  logo = PhotoImage(file='./imgs/logotipo.preto.png')
+  lbllogo = Label(tela, image=logo, bg=config.COR_BG)
+  lbllogo.place(x=140, y=10)
+  img2 = PhotoImage(file='./imgs/onda2.png')
+  lblimg2 = Label(tela, image=img2, bg=config.COR_BG)
+  lblimg2.place(x=0, y=180)
+
+
+  labelTask = Message(tela, text="Ajude a Dona Creuza a fazer as compras", font="Arial 14 bold", bg=config.COR_BG, width=220, justify=CENTER)
+  labelTask.place(x=190, y=140)
+  progresso = ttk.Progressbar(tela, orient=HORIZONTAL, length=200, mode='determinate')
+  progresso.place(x=190, y=230)
+  labelPontos = Label(tela, text='', bg=config.COR_BG)
+  
+  clickButton = Button(tela, text='CLIQUE!', command=lambda: aumentarProgresso(progresso, config.TASKS, labelPontos, labelProdutos, labelTask, usuario))
+
+  clickButton.place(x=260, y=260)
+  labelPontos.place(x=235, y=290)
 
   tela.mainloop()
-
 

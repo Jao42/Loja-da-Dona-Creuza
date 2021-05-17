ad5c37c31227605cf8df1ddc789158d822322c98
Jao42 <joaovictormedeiros41@gmail.com>
2021-05-17 07:12:50 -0300

    remake tela produtos

--- tela_produtos.py
+++ tela_produtos.py
@@ -3,25 +3,42 @@ from tkinter import ttk
 from centralizar_tela import centralizarTela
 from tela_minigame import telaMinigame
 from produtos import tableProdutos, comprarProduto, promocao
-import sqlite3
 import config
 
 
 def telaProdutos(usuario, pontos):
   tela = Toplevel()
   tela.configure(bg=config.COR_BG)
-  centralizarTela(tela, 600, 500)
-  tela.title('estoque')
+  tela.geometry('1366x768')
+  tela.title('Estoque')
 
-  label_usuario_pontos = Label(tela, text=f"{usuario} --> {pontos}", bg=config.COR_BG, width=20)
-  botaoMinigame = Button(tela, text='GANHAR PONTOS', width=15, command= lambda:telaMinigame(usuario, label_usuario_pontos))
-  botaoMinigame.pack(anchor=W)
+  tela.resizable(False, False)
+
+  img = PhotoImage(file='./imgs/circulop.png')
+  lblimg = Label(tela, image=img, bg=config.COR_BG)
+  lblimg.place(x=0, y=0)
+  img2 = PhotoImage(file='./imgs/onda.png')
+  lblimg2 = Label(tela, image=img2, bg=config.COR_BG)
+  lblimg2.place(x=665, y=370)
+
+  label_usuario_pontos = Label(tela, text=f"{usuario} → {pontos}", font='Arial 15 bold', bg=config.COR_BG, width=20)
+
+  botaoMinigame = Button(
+      tela,
+      text='GANHAR PONTOS',
+      font='Arial 12',
+      height=2, 
+      width=20,
+      command=lambda: telaMinigame(usuario, label_usuario_pontos))
+
+  botaoMinigame.place(x=470, y=600)
   label_usuario_pontos.pack()
 
   #relief --> estilo da borda
 
   body = Frame(tela, borderwidth=2, relief='solid') 
-  tabelaItems=['nome', 'preco', 'promocao', 'qtd']
+  body.pack(side=TOP)
+  tabelaItems = ['nome', 'preco', 'promocao', 'qtd']
 
 #selectmode -> quantos items seleciona com mouse
 #treeView heigth -> quantas rows
@@ -29,7 +46,7 @@ def telaProdutos(usuario, pontos):
   scrollbarX = Scrollbar(body, orient=HORIZONTAL)
 
   tree = ttk.Treeview(
-      body, columns=(tabelaItems[0], tabelaItems[1], tabelaItems[2], tabelaItems[3]), selectmode="extended", height=5,
+      body, columns=tabelaItems, selectmode="extended", height=8,
       yscrollcommand=scrollbarY.set, xscrollcommand=scrollbarX.set)
 
   scrollbarY.config(command=tree.yview)
@@ -40,21 +57,27 @@ def telaProdutos(usuario, pontos):
   style = ttk.Style(body)
   style.configure('Treeview', rowheight=60)
 
-  tree.heading(tabelaItems[0], text=tabelaItems[0])
-  tree.heading(tabelaItems[1], text=tabelaItems[1])
-  tree.heading(tabelaItems[2], text=tabelaItems[2])
-  tree.heading(tabelaItems[3], text=tabelaItems[3])
+  for i in tabelaItems:
+    tree.heading(i, text=i)
 
   tree.column('#0', minwidth=0, width=0)
-  tree.column('#1', anchor="center", minwidth=0, width=200)
+  tree.column('#1', anchor="center", minwidth=0, width=250)
   tree.column('#2', anchor="center", minwidth=0, width=150)
-  tree.column('#3', anchor="center", minwidth=0, width=100)
-  tree.column('#4', anchor="center", minwidth=0, width=50)
+  tree.column('#3', anchor="center", minwidth=0, width=150)
+  tree.column('#4', anchor="center", minwidth=0, width=100)
 
-  horaPromocao = promocao(8, 18)
+  horaPromocao = promocao(17, 23)
   temProdutoPromocao = tableProdutos(tree, body, horaPromocao)
 
-  Button(tela, text="COMPRAR", command = lambda:comprarProduto(tree, label_usuario_pontos, usuario)).pack(side=BOTTOM)
+  comprarButton = Button(
+      tela,
+      text="COMPRAR",
+      font='Arial 12',
+      height=2,
+      width=20,
+      command=lambda: comprarProduto(tree, label_usuario_pontos, usuario))
+
+  comprarButton.place(x=700, y=600)
   
   promocaoLabel = Label(tela, bg=config.COR_BG)
   promocaoLabel.pack()

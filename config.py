ba687a8a0580ec69909b56dba7f81570b832f129
Jao42 <joaovictormedeiros41@gmail.com>
2021-05-17 07:11:14 -0300

    lista de tasks

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 COR_BG = '#9ddde0'
 COR_BG2 = '#ffffff'
 COR_BG3 = '#000000'
+
+TASKS = [
+    'fazer as compras',
+    'tricotar',
+    'trocar o pneu do carro',
+    'atravessar a rua',
+    'aumentar o brilho do celular'
+    ]
